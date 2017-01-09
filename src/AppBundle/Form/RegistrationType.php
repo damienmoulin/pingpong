b6cca5d35786968f2147ba595dfa10ad03460704
damienmoulin <damien.moulin1@gmail.com>
2017-01-09 14:16:06 +0100

    [User] RegisterForm overrite

---
+++ src/AppBundle/Form/RegistrationType.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: damien
+ * Date: 09/01/17
+ * Time: 13:36
+ */
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class RegistrationType extends  AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname',TextType::class,
+                [
+                    'label' => 'Prénom'
+                ])
+            ->add('lastname',TextType::class,
+                [
+                    'label' => 'Nom'
+                ])
+            ->add('structurename',TextType::class,
+                [
+                    'label' => 'Structure'
+                ])
+            ->add('structuretype',ChoiceType::class,
+                [
+                    'choices' =>
+                        [
+                            'École'  => "École",
+                            'Agence' => 'Agence'
+                        ],
+                    'label' => 'Type de structure'
+                ])
+            ->add('teamname', TextType::class,
+                [
+                    'label' => 'Team'
+                ])
+            ->add('phonenumber', TextType::class,
+                [
+                    'label' => 'Télephone'
+                ])
+            ;
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+}
