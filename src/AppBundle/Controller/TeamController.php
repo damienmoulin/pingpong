a634344d882eeac6c3d870e811a8ca13379839d3
damienmoulin <damien.moulin1@gmail.com>
2017-01-10 13:23:33 +0100

    [Team] Accept & Decline lien pour les participants

--- src/AppBundle/Controller/TeamController.php
+++ src/AppBundle/Controller/TeamController.php
@@ -12,6 +12,7 @@ use AppBundle\Entity\Player;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Route("/team", name="team")
@@ -27,8 +28,6 @@ class TeamController extends Controller
 
         $player = new Player($user);
 
-        $players = $user->getPlayers();
-
         $form = $this->get('form.factory')->create('AppBundle\Form\PlayerType', $player);
 
         $form->handleRequest($request);
@@ -37,14 +36,78 @@ class TeamController extends Controller
             $em = $this->getDoctrine()->getManager();
             $em->persist($player);
 
-            $em->flush();
+            $link = sha1(uniqid(mt_rand(), true));
+            $player->setPrivatekey($link);
 
-            $player = new Player($user);
-            $form = $this->get('form.factory')->create('AppBundle\Form\PlayerType', $player);
+            $em->flush();
+            
+            return $this->redirect($this->generateUrl('team'));
         }
+
         return $this->render('team/index.html.twig', [
             'form' => $form->createView(),
             'user' => $user
         ]);
+    }
+
+    /**
+     * @param $key
+     * @return renderView
+     * @Route("/accept/{key}", name="team_accept_player")
+     */
+    public function acceptPlayerAction($key)
+    {
+        $player = $this->getDoctrine()->getRepository('AppBundle:Player')->findOneBy(
+            [
+                'privatekey' => $key
+            ]);
+
+        $message = 'Vous avez déja refusé l\'invitation , vous ne pouvez plus accepté de participer';
+
+        if (($player != null) && ($player->getStatus() != 0)) {
+            $player->accept();
+            $this->flush($player);
+
+            $message = 'Vous avez accepté de participer au tournoi';
+        }
+
+        return $this->render('team/confirm_decline.html.twig', [
+            'message' => $message
+        ]);
+    }
+
+    /**
+     * @param $key
+     * @return renderView
+     * @Route("/decline/{key}", name="team_decline_player")
+     */
+    public function declinePlayerAction($key)
+    {
+        $player = $this->getDoctrine()->getRepository('AppBundle:Player')->findOneBy(
+            [
+                'privatekey' => $key
+            ]);
+
+        $message = 'Vous avez déja accepter l\'invitation , vous ne pouvez plus refuser de participer';
+
+        if (($player != null) && ($player->getStatus() != 1)) {
+            $player->decline();
+            $this->flush($player);
+            $message = 'Vous avez refusé de participer au tournoi';
+        }
+
+        return $this->render('team/confirm_decline.html.twig',[
+            'message' => $message
+        ]);
+    }
+
+    /**
+     * @param $entity
+     */
+    public function flush($entity)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($entity);
+        $em->flush();
     }
 }
